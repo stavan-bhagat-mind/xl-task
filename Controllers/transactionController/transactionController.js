7f0aafe1678d34d07b706f75eb95a10c29361da7
mind <mind@minds-Mac-mini.local>
2024-12-12 18:40:19 +0530

    fix: logic

--- Controllers/transactionController/transactionController.js
+++ Controllers/transactionController/transactionController.js
@@ -1,116 +1,167 @@
 const { Models } = require("../../models");
 const { readXLData } = require("../../utility/helper");
 const { Op } = require("sequelize");
+const { http } = require("../../utility/constant");
 
 module.exports.createTransaction = async (req, res) => {
   try {
-    // console.log("hello", req.file.path);
-    // if (!req.file) {
-    //   return res.status(400).json({ error: "No  file uploaded" });
-    // }
-    // const fileUrl = req.file.path;
-    // console.log(fileUrl);
     const userId = req.userId;
-    const data = await readXLData(
+
+    // Process the Excel file
+    const result = await readXLData(
       "/Users/mind/Documents/demo/xl task/xl-task.xlsx",
-      res,
       userId
     );
 
-    // if (errors.length > 0) {
-    //   return res.send({
-    //     errors,
-    //     message: "Failed",
-    //   });
-    // }
+    if (!result.success) {
+      const errorMessage =
+        result.errors.length >= 100
+          ? "More than 100 errors found. Please correct the inputs."
+          : "Errors found";
 
-    // // Step 1: Fetch existing accounts
+      return res.status(http.BAD_REQUEST.code).send({
+        success: false,
+        data: result.errors,
+        message: errorMessage,
+      });
+    }
+
+    // Get account data for the user
     const accountData = await Models.Account.findAll({
       where: {
-        name: { [Op.in]: data[0].accountNames },
+        name: { [Models.Sequelize.Op.in]: result.values[0].accountNames },
         user_id: userId,
       },
     });
 
-    // Step 2: Create a mapping of existing accounts
+    // Create account mapping
     const accountMap = {};
     accountData.forEach((account) => {
       accountMap[account.dataValues.name] = account.dataValues.id;
     });
-    console.log("data------", data);
-    console.log("accountData", accountData);
-    console.log("accountMap", accountMap);
 
-    // const createdTransactions = await Models.Transaction.findAndCountAll({})
-    // // Step 3: Identify missing accounts
-    // const missingAccountNames = data[0].accountNames.filter(
-    //   (name) => !accountMap[name]
-    // );
+    // Create transactions
+    const transactions = result.values.slice(1).map((transaction) => ({
+      account_id: accountMap[transaction.name],
+      user_id: userId,
+      category: transaction.category,
+      date: transaction.date,
+      amount: transaction.amount,
+    }));
 
-    // // Step 4: Insert missing accounts
-    // const newAccounts = missingAccountNames.map((name) => ({
-    //   name,
-    //   user_id: userId,
-    // }));
+    await Models.Transaction.bulkCreate(transactions);
 
-    // // Insert new accounts if there are any
-    // if (newAccounts.length > 0) {
-    //   const createdAccounts = await Models.Account.bulkCreate(newAccounts);
-    //   // Update the accountMap with newly created accounts
-    //   createdAccounts.forEach((account) => {
-    //     accountMap[account.dataValues.name] = account.dataValues.id;
-    //   });
-    // }
-
-    // // Step 5: Remove the first element (accountNames) from data
-    // data.shift();
-
-    // // Step 6: Transform the transaction data to include account IDs
-    // const transactionData = data.map((transaction) => {
-    //   return {
-    //     account_id: accountMap[transaction.name],
-    //     category: transaction.category,
-    //     date: transaction.date,
-    //     amount: transaction.amount,
-    //     created_by: userId,
-    //   };
-    // });
-
-    // // Step 7: Perform the bulk insert for transactions
-    // const createdTransactions = await Models.Transaction.bulkCreate(
-    //   transactionData
-    // );
-    // console.log("Created Transactions:", createdTransactions);
-    // --------------------------
-    // console.log(data[0].accountNames);
-    // const accountData = await Models.Account.findAll({
-    //   where: {
-    //     name: { [Op.in]: data[0].accountNames },
-    //     user_id: userId,
-    //   },
-    // });
-    // console.log("accountData", accountData);
-    // const accountDetails = accountData.map((account) => {
-    //   return {
-    //     id: account.dataValues.id,
-    //     name: account.dataValues.name,
-    //   };
-    // });
-    // console.log("accountData", accountDetails);
-    // data.shift();
-    // const transactionData = await Models.Transaction.bulkCreate(data);
-    // console.log("transactionData", transactionData);
-    // const data = await Models.User.create({ name });
-
-    return res.send({
-      data,
-      // accountData,
-      message: "success",
+    return res.status(http.OK.code).send({
+      success: true,
+      message: "Transactions created successfully",
+      data: {
+        totalProcessed: transactions.length,
+        accounts: Object.keys(accountMap).length,
+      },
     });
-  } catch (e) {
-    // return res.send({
-    //   data: null,
-    //   message: "Something went wrong.",
-    // });
+  } catch (error) {
+    console.error("Error creating transaction:", error);
+    return res.status(http.INTERNAL_SERVER_ERROR.code).send({
+      success: false,
+      message: "Error creating transaction",
+      error: error.message,
+    });
   }
 };
+// if (errors.length > 0) {
+//   return res.send({
+//     errors,
+//     message: "Failed",
+//   });
+// }
+
+// // // Step 1: Fetch existing accounts
+// const accountData = await Models.Account.findAll({
+//   where: {
+//     name: { [Op.in]: data[0].accountNames },
+//     user_id: userId,
+//   },
+// });
+
+// // Step 2: Create a mapping of existing accounts
+// const accountMap = {};
+// accountData.forEach((account) => {
+//   accountMap[account.dataValues.name] = account.dataValues.id;
+// });
+// console.log("data------", data);
+// console.log("accountData", accountData);
+// console.log("accountMap", accountMap);
+
+// const createdTransactions = await Models.Transaction.findAndCountAll({})
+// // Step 3: Identify missing accounts
+// const missingAccountNames = data[0].accountNames.filter(
+//   (name) => !accountMap[name]
+// );
+
+// // Step 4: Insert missing accounts
+// const newAccounts = missingAccountNames.map((name) => ({
+//   name,
+//   user_id: userId,
+// }));
+
+// // Insert new accounts if there are any
+// if (newAccounts.length > 0) {
+//   const createdAccounts = await Models.Account.bulkCreate(newAccounts);
+//   // Update the accountMap with newly created accounts
+//   createdAccounts.forEach((account) => {
+//     accountMap[account.dataValues.name] = account.dataValues.id;
+//   });
+// }
+
+// // Step 5: Remove the first element (accountNames) from data
+// data.shift();
+
+// // Step 6: Transform the transaction data to include account IDs
+// const transactionData = data.map((transaction) => {
+//   return {
+//     account_id: accountMap[transaction.name],
+//     category: transaction.category,
+//     date: transaction.date,
+//     amount: transaction.amount,
+//     created_by: userId,
+//   };
+// });
+
+// // Step 7: Perform the bulk insert for transactions
+// const createdTransactions = await Models.Transaction.bulkCreate(
+//   transactionData
+// );
+// console.log("Created Transactions:", createdTransactions);
+// --------------------------
+// console.log(data[0].accountNames);
+// const accountData = await Models.Account.findAll({
+//   where: {
+//     name: { [Op.in]: data[0].accountNames },
+//     user_id: userId,
+//   },
+// });
+// console.log("accountData", accountData);
+// const accountDetails = accountData.map((account) => {
+//   return {
+//     id: account.dataValues.id,
+//     name: account.dataValues.name,
+//   };
+// });
+// console.log("accountData", accountDetails);
+// data.shift();
+// const transactionData = await Models.Transaction.bulkCreate(data);
+// console.log("transactionData", transactionData);
+// const data = await Models.User.create({ name });
+
+//     return res.send({
+//       data,
+//       // accountData,
+//       message: "success",
+//     });
+//   } catch (e) {
+//     // return res.send({
+//     //   data: null,
+//     //   message: "Something went wrong.",
+//     // });
+//   }
+// };
--- utility/helper.js
+++ utility/helper.js
@@ -8,128 +8,269 @@ const { salt } = require("../utility/constant");
 const { validateTransaction } = require("../validations/transactionValidation");
 const { http } = require("../utility/constant");
 
-module.exports.readXLData = async (file, res, userId) => {
+const excelDateToJSDate = (excelDate) => {
+  // Excel dates are number of days since Dec 30, 1899
+  const utcDays = Math.floor(excelDate - 25569);
+  const utcValue = utcDays * 86400 * 1000;
+  return new Date(utcValue);
+};
+
+module.exports.readXLData = async (file, userId) => {
   try {
-    const CHUNK_SIZE = 2;
     const errors = [];
     const maxErrors = 100;
     const uniqueTransactions = new Set();
     const values = [{ accountNames: [] }];
-    var workbook = XLSX.readFile(file);
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const xlData = XLSX.utils.sheet_to_json(worksheet, { raw: false });
-    console.log("xlData", xlData);
 
-    for (let index = 0; index < xlData.length; index += CHUNK_SIZE) {
-      const chunk = xlData.slice(index, index + CHUNK_SIZE);
-      for (const transaction of chunk) {
-        if (errors.length >= maxErrors) {
-          break;
-        }
+    const workbook = XLSX.readFile(file, {
+      stream: true,
+      sheets: [0],
+      density: true,
+    });
 
-        if (transaction.date) {
-          const [month, day, year] = transaction.date.split("-");
-          transaction.date = new Date(Date.UTC(year, month - 1, day));
-        }
-        const { success, error, value } = validateTransaction(
-          transaction,
-          uniqueTransactions
-        );
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const range = XLSX.utils.decode_range(worksheet["!ref"]);
 
-        if (!success) {
-          errors.push({
-            row: index + 1,
-            details: [
-              {
-                message: error.message.replace(/\"/g, ""),
-              },
-            ],
-          });
-        } else {
-          values[0].accountNames.push(value.name);
-          values.push(value);
+    // Read headers
+    const headers = {};
+    const headerRow = range.s.r;
+    for (let col = range.s.c; col <= range.e.c; col++) {
+      const cellAddress = XLSX.utils.encode_cell({ r: headerRow, c: col });
+      const cell = worksheet[cellAddress];
+      if (cell) headers[col] = cell.v;
+    }
+
+    // Process rows in batches
+    const batchSize = 1000;
+    let currentBatch = [];
+
+    for (let row = range.s.r + 1; row <= range.e.r; row++) {
+      const rowData = {};
+
+      for (let col = range.s.c; col <= range.e.c; col++) {
+        const cellAddress = XLSX.utils.encode_cell({ r: row, c: col });
+        const cell = worksheet[cellAddress];
+        if (cell && headers[col]) {
+          rowData[headers[col]] = cell.v;
         }
       }
-      if (errors.length >= maxErrors) {
-        break;
+
+      if (Object.keys(rowData).length === 0) continue;
+
+      if (rowData.date) {
+        rowData.date = excelDateToJSDate(rowData.date);
       }
-      if (values.length > 0) {
-        const conditions = values.slice(1).map((t) => ({
-          [Models.Sequelize.Op.and]: {
-            date: t.date,
-            amount: t.amount,
-            category: t.category,
-          },
-        }));
-        const existingTransactions = await Models.Transaction.findAll({
-          where: {
-            user_id: userId,
-            [Models.Sequelize.Op.or]: conditions,
-          },
-          include: [
-            {
-              model: Models.Account,
-              attributes: ["name"],
-            },
-          ],
-          attributes: ["account_id", "category", "date", "amount", "user_id"],
-          raw: true,
+
+      const { success, error, value } = validateTransaction(
+        rowData,
+        uniqueTransactions
+      );
+
+      if (!success) {
+        errors.push({
+          row: row + 1,
+          details: [{ message: error.message.replace(/\"/g, "") }],
         });
-
-        // Create a map of existing transactions for quick looku
-        existingTransactions.forEach((t) => {
-          const b = `${t["Account.name"]}-${t.category}-${new Date(
-            t.date
-          ).toISOString()}-${t.amount}`;
-        console.log(  uniqueTransactions.has(b))
-          if (
-            uniqueTransactions.has(
-              `${t["Account.name"]}-${t.category}-${new Date(
-                t.date
-              ).toISOString()}-${t.amount}`
-            )
-          ) {
-            errors.push({
-              row: index + idx + 1,
-              details: [
-                {
-                  message: `Duplicate transaction found in database for account: ${
-                    transaction.name
-                  }, date: ${transaction.date.toISOString()}, amount: ${
-                    transaction.amount
-                  }, category: ${transaction.category}`,
-                },
-              ],
-            });
-          }
-        });
-
-        // Check for duplicates and add errors
+      } else {
+        values[0].accountNames.push(value.name);
+        currentBatch.push(value);
       }
+
+      if (currentBatch.length >= batchSize) {
+        await processBatch(currentBatch, userId, errors, row - batchSize + 1);
+        currentBatch = [];
+      }
+
+      if (errors.length >= maxErrors) break;
     }
-    if (errors.length > 0) {
-      const errorMessage =
-        errors.length >= maxErrors
-          ? "More than 100 errors found. Please correct the inputs."
-          : "Errors found";
-      return res.status(http.BAD_REQUEST.code).send({
-        success: false,
-        data: errors,
-        message: errorMessage,
-      });
-    } else {
-      return values;
+
+    if (currentBatch.length > 0) {
+      await processBatch(
+        currentBatch,
+        userId,
+        errors,
+        range.e.r - currentBatch.length + 1
+      );
     }
-  } catch (e) {
-    if (e.message) {
-      return res.status(http.BAD_REQUEST.code).send({
-        success: false,
-        data: null,
-        message: e.message,
-      });
-    }
+
+    return {
+      success: errors.length === 0,
+      errors,
+      values,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      errors: [{ message: error.message || "Error processing Excel file" }],
+      values: null,
+    };
   }
 };
+
+const processBatch = async (batch, userId, errors, startRow) => {
+  try {
+    const conditions = batch.map((t) => ({
+      [Models.Sequelize.Op.and]: {
+        date: t.date,
+        amount: t.amount,
+        category: t.category,
+      },
+    }));
+
+    const existingTransactions = await Models.Transaction.findAll({
+      where: {
+        user_id: userId,
+        [Models.Sequelize.Op.or]: conditions,
+      },
+      include: [
+        {
+          model: Models.Account,
+          attributes: ["name"],
+        },
+      ],
+      attributes: ["account_id", "category", "date", "amount", "user_id"],
+      raw: true,
+    });
+
+    existingTransactions.forEach((transaction, idx) => {
+      const key = `${transaction["Account.name"]}-${
+        transaction.category
+      }-${new Date(transaction.date).toISOString()}-${transaction.amount}`;
+
+      if (uniqueTransactions.has(key)) {
+        errors.push({
+          row: startRow + idx,
+          details: [
+            {
+              message: `Duplicate transaction found in database for account: ${
+                transaction["Account.name"]
+              }, date: ${new Date(transaction.date).toISOString()}, amount: ${
+                transaction.amount
+              }, category: ${transaction.category}`,
+            },
+          ],
+        });
+      }
+    });
+  } catch (error) {
+    errors.push({
+      row: startRow,
+      details: [{ message: "Error processing batch: " + error.message }],
+    });
+  }
+};
+
+//     for (let index = 0; index < xlData.length; index += CHUNK_SIZE) {
+//       const chunk = xlData.slice(index, index + CHUNK_SIZE);
+//       for (const transaction of chunk) {
+//         if (errors.length >= maxErrors) {
+//           break;
+//         }
+
+//         if (transaction.date) {
+//           const [month, day, year] = transaction.date.split("-");
+//           transaction.date = new Date(Date.UTC(year, month - 1, day));
+//         }
+//         const { success, error, value } = validateTransaction(
+//           transaction,
+//           uniqueTransactions
+//         );
+
+//         if (!success) {
+//           errors.push({
+//             row: index + 1,
+//             details: [
+//               {
+//                 message: error.message.replace(/\"/g, ""),
+//               },
+//             ],
+//           });
+//         } else {
+//           values[0].accountNames.push(value.name);
+//           values.push(value);
+//         }
+//       }
+//       if (errors.length >= maxErrors) {
+//         break;
+//       }
+//       if (values.length > 0) {
+//         const conditions = values.slice(1).map((t) => ({
+//           [Models.Sequelize.Op.and]: {
+//             date: t.date,
+//             amount: t.amount,
+//             category: t.category,
+//           },
+//         }));
+//         const existingTransactions = await Models.Transaction.findAll({
+//           where: {
+//             user_id: userId,
+//             [Models.Sequelize.Op.or]: conditions,
+//           },
+//           include: [
+//             {
+//               model: Models.Account,
+//               attributes: ["name"],
+//             },
+//           ],
+//           attributes: ["account_id", "category", "date", "amount", "user_id"],
+//           raw: true,
+//         });
+
+//         // Create a map of existing transactions for quick looku
+//         existingTransactions.forEach((t) => {
+//           const b = `${t["Account.name"]}-${t.category}-${new Date(
+//             t.date
+//           ).toISOString()}-${t.amount}`;
+//           console.log(uniqueTransactions.has(b));
+//           if (
+//             uniqueTransactions.has(
+//               `${t["Account.name"]}-${t.category}-${new Date(
+//                 t.date
+//               ).toISOString()}-${t.amount}`
+//             )
+//           ) {
+//             errors.push({
+//               row: index + idx + 1,
+//               details: [
+//                 {
+//                   message: `Duplicate transaction found in database for account: ${
+//                     transaction.name
+//                   }, date: ${transaction.date.toISOString()}, amount: ${
+//                     transaction.amount
+//                   }, category: ${transaction.category}`,
+//                 },
+//               ],
+//             });
+//           }
+//         });
+
+//         // Check for duplicates and add errors
+//       }
+//     }
+//     if (errors.length > 0) {
+//       const errorMessage =
+//         errors.length >= maxErrors
+//           ? "More than 100 errors found. Please correct the inputs."
+//           : "Errors found";
+//       return res.status(http.BAD_REQUEST.code).send({
+//         success: false,
+//         data: errors,
+//         message: errorMessage,
+//       });
+//     } else {
+//       return values;
+//     }
+//   } catch (e) {
+//     if (e.message) {
+//       return res.status(http.BAD_REQUEST.code).send({
+//         success: false,
+//         data: null,
+//         message: e.message,
+//       });
+//     }
+//   }
+// };
 
 module.exports.hashConvert = async (plainPassword) => {
   return new Promise((resolve, reject) => {
